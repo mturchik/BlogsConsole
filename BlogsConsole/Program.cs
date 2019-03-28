bf1701f4634489113c97d97dd725bb7f48a9fc3c
mturchik <mturchik@my.wctc.edu>
2019-03-27 23:22:16 -0500

    Blog/Console Application
    1) Added Menu for Displaying/Adding/Posting
    2) Added personal Validate.cs file
    3) Added Post functionality

--- BlogsConsole/Program.cs
+++ BlogsConsole/Program.cs
@@ -1,7 +1,9 @@
 ﻿using NLog;
 using BlogsConsole.Models;
 using System;
+using System.Data.Entity.Core.Metadata.Edm;
 using System.Linq;
+using System.Reflection;
 
 namespace BlogsConsole
 {
@@ -13,33 +15,72 @@ namespace BlogsConsole
             logger.Info("Program started");
             try
             {
-
-                // Create and save a new Blog
-                Console.Write("Enter a name for a new Blog: ");
-                var name = Console.ReadLine();
-
-                var blog = new Blog { Name = name };
-
-                var db = new BloggingContext();
-                db.AddBlog(blog);
-                logger.Info("Blog added - {name}", name);
-
-                // Display all Blogs from the database
-                var query = db.Blogs.OrderBy(b => b.Name);
-
-                Console.WriteLine("All blogs in the database:");
-                foreach (var item in query)
+                bool cont = true;
+                while (cont)
                 {
-                    Console.WriteLine(item.Name);
+                    var db = new BloggingContext();
+                    Console.Write("Welcome to the Blog/Console~\n" +
+                                  "1. Display all Blogs\n" +
+                                  "2. Add Blog\n" +
+                                  "3. Create Post\n" +
+                                  "4. Exit\n" +
+                                  "===");
+                    string input = Console.ReadLine();
+                    switch (Validate.ValidateMenuSelection(input, 4))
+                    {
+                        case "1":
+                            // Display all Blogs from the database
+                            var query = db.Blogs.OrderBy(b => b.Name);
+                            Console.WriteLine("All blogs in the database:");
+                            foreach (var item in query)
+                            {
+                                Console.WriteLine(item.Name);
+                            }
+                            break;
+                        case "2":
+                            // Create and save a new Blog
+                            Console.Write("Enter a name for a new Blog: ");
+                            var name = Console.ReadLine();
+
+                            var blog = new Blog { Name = name };
+                            
+                            db.AddBlog(blog);
+                            logger.Info("Blog added - {name}", name);
+                            break;
+                        case "3":
+                            var listBlogs = db.Blogs.OrderBy(b => b.BlogId);
+                            Console.WriteLine("All blogs in the database:");
+                            foreach (var item in listBlogs)
+                            {
+                                Console.WriteLine("BlogId: " + item.BlogId + " - BlogName: " + item.Name);
+                            }
+                            Console.Write("Enter the BlogId of the Blog to post to:\n" +
+                                          "=");
+                            input = Console.ReadLine();
+                            var intInput = int.Parse(input);
+
+                            Blog chosenBlog = db.Blogs.Find(intInput);
+
+                            Console.Write("Enter post title:\n" +
+                                              "=");
+                            var title = Validate.ValidateBlank(Console.ReadLine());
+                            Console.Write("Enter post content:\n" +
+                                          "=");
+                            var content = Validate.ValidateBlank(Console.ReadLine());
+                            var post = new Post() { Title = title, Content = content, BlogId = chosenBlog.BlogId , Blog = chosenBlog };
+                            db.AddPost(post);
+                            logger.Info("Post added - {name}", post.PostId);
+                            break;
+                        case "4":
+                            cont = false;
+                            break;
+                    }
                 }
             }
             catch (Exception ex)
             {
                 logger.Error(ex.Message);
             }
-
-            Console.WriteLine("Press enter to quit");
-            string x = Console.ReadLine();
 
             logger.Info("Program ended");
         }
